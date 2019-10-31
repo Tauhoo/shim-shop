8b0908f0edba6710cbee82e203e32790cdc0ba51
wachirawit wacharak <tauhoo@wachirawits-MacBook-Pro.local>
2019-11-01 01:01:20 +0700

    extract file

---
+++ precache-manifest.085fcbfe6cf5151bc90e08c780f6d6c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c7eefa9bacec1f188bef57ad25c7d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb7ef9c9ce1aabfc660",
+    "url": "/static/css/main.a14ca5b6.chunk.css"
+  },
+  {
+    "revision": "2555c836d44515e9e5c0",
+    "url": "/static/js/2.5fcdae99.chunk.js"
+  },
+  {
+    "revision": "4cb7ef9c9ce1aabfc660",
+    "url": "/static/js/main.9243d087.chunk.js"
+  },
+  {
+    "revision": "cf612ab2b6c2992e1df8",
+    "url": "/static/js/runtime-main.9d059866.js"
+  }
+]);
--- src/utils/fetch.js
+++
@@ -1,5 +0,0 @@
-export default async () =>
-  fetch("https://panjs.com/ywc.json")
-    .then(async res => await res.json())
-    .then(result => ({ status: "success", result }))
-    .catch(detail => ({ status: "error", detail }))
